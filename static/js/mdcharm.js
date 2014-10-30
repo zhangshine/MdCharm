215eabe3a98a3b838ad44db5df1a66581243902f
zhangshine <zhangshine0125@gmail.com>
2014-10-30 14:35:16 +0800

    download link

--- static/js/mdcharm.js
+++ static/js/mdcharm.js
@@ -6,17 +6,11 @@ function check_os()
     unix = (navigator.userAgent.indexOf("Unix", 0) != -1) ? 1 : 0;
     if (windows)
     {
-        document.getElementById("os_area").innerHTML =
-            "<a href=\"https://github.com/zhangshine/MdCharm/releases/download/1.0.0/MdCharm-Setup.exe\" class=\"btn btn-primary btn-large\" onclick=\"_gaq.push(['_trackEvent', 'Download-Windows', 'Click', 'IndexPage']);\"><span class=\"large\">Download MdCharm For Windows</span><span class=\"small\">(V1.1.6)</span><br><span class=\"small\" > Free Trial</span><br><span class=\"small\">Windows XP, Vista, and 7</span></a>"
+        document.getElementById("platform").innerHTML = 'Download MdCharm For Windows';
+        document.getElementById("platform_version").innerHTML = 'Windows XP, Vista, and 7';
         //do noting
-    }
-//    } else if(mac)
-//    {
-//        document.getElementById("os_area").innerHTML =
-//            "<a class=\"btn btn-primary btn-large\"><span class=\"large\">Download MdCharm For Windows</span><span class=\"small\">(V0.1.0)</span><br><span class=\"small\">Windows XP, Vista, and 7</span></a>"
-//    }
-    else {
-        document.getElementById("os_area").innerHTML =
-            "<a href=\"/release/1.1.6/mdcharm_1.1.6_i386.deb\" class=\"btn btn-primary btn-large\" onclick='_gaq.push(['_trackEvent', 'Download-Ubuntu', 'Click', 'IndexPage']);'><span class=\"large\">Download MdCharm For Ubuntu</span><span class=\"small\">(V1.1.6)Free</span><br><span class=\"small\">Ubuntu 12.04</span></a>"
+    } else {
+        document.getElementById("platform").innerHTML = 'Download MdCharm For Linux';
+        document.getElementById("platform_version").innerHTML = 'Ubuntu 14.04 64bit';
     }
 }
